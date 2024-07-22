925f41e8c8792d254a168aee82c85382e9616531
Dirceusljr <dirceusljr@gmail.com>
2024-07-22 18:16:37 -0300

    CRUD de livros de um usuário

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,6 +1,8 @@
 import LivrosServices from "../services/livrosServices.js";
+import UsuariosServices from "../services/usuariosServices.js";
 
 const livrosServices = new LivrosServices();
+const usuariosServices = new UsuariosServices();
 
 class LivrosController {
   static async cadastrarLivro(req, res) {
@@ -100,6 +102,125 @@ static async deletarLivro(req, res) {
         res.status(500).send({ erro: erro.message });
     }
 }
+
+static async buscarLivrosPorUsuarioId(req, res) {
+    const { usuarioId } = req.params;
+
+    if (!usuarioId) {
+        throw new Error('Id do usuário é obrigatório');
+    }
+
+    try {
+        const listaLivros = await livrosServices.buscarLivrosPorUsuarioId({ usuarioId });
+
+        res.status(200).send(listaLivros);
+    } catch (erro) {
+        res.status(500).send({ erro: erro.message });
+    }
+}
+
+static async cadastrarLivroParaUsuario(req, res) {
+    const { usuarioId } = req.params;
+    let { titulo, autor, linkCapa, editora, genero, paginas } =
+      req.body;
+
+    if (!titulo || !autor || !usuarioId) {
+      throw new Error("Campos obrigatórios não preenchidos.");
+    }
+
+    const usuario = await usuariosServices.buscarUsuarioPorId({ id: usuarioId });
+
+    if (!usuario) {
+      throw new Error("Usuário não encontrado.");
+    }
+
+    if (paginas) {
+      paginas = parseInt(paginas);
+    }
+
+    try {
+      const novoLivro = await livrosServices.cadastrarLivro({
+        titulo,
+        autor,
+        usuarioId,
+        linkCapa,
+        editora,
+        genero,
+        paginas
+      })
+
+      res.status(201).send({message:'Registro criado com sucesso', novoLivro})
+    } catch (erro) {
+      throw new Error("Houve algum erro no banco de dados.");
+    }
+  }
+
+  static async editarLivroDoUsuario(req, res) {
+    const { usuarioId, id } = req.params;
+    let { titulo, autor, linkCapa, editora, genero, paginas, avaliacao, disponibilidade } =
+      req.body;
+
+      console.log()
+
+    if (!id || !usuarioId) {
+      throw new Error("Campos obrigatórios não preenchidos.");
+    }
+
+    const usuario = await usuariosServices.buscarUsuarioPorId({ id: usuarioId });
+
+    if (!usuario) {
+      throw new Error("Usuário não encontrado.");
+    }
+
+    if (paginas) {
+      paginas = parseInt(paginas);
+    }
+
+    try {
+      const livroAtualizado = await livrosServices.editarLivro({
+        id,
+        titulo,
+        autor,
+        usuarioId,
+        linkCapa,
+        editora,
+        genero,
+        paginas,
+        avaliacao, 
+        disponibilidade
+      })
+
+      res.status(201).send(livroAtualizado)
+    } catch (erro) {
+      throw new Error("Houve algum erro no banco de dados.");
+    }
+  }
+
+  static async deletarLivroDoUsuario(req, res) {
+    const { usuarioId, id } = req.params;
+
+    if (!id || !usuarioId) {
+        throw new Error("Campos obrigatórios não preenchidos.");
+      }
+  
+      const usuario = await usuariosServices.buscarUsuarioPorId({ id: usuarioId });
+  
+      if (!usuario) {
+        throw new Error("Usuário não encontrado.");
+      }
+
+      try {
+        const livroDeletado = await livrosServices.deletarLivro({ id });
+
+        res.status(200).send({
+            message: "Livro deletado com sucesso!",
+            livroDeletado: livroDeletado
+        })
+        
+    } catch (erro) {
+        res.status(500).send({ erro: erro.message });
+    }
+  }
 }
 
 export default LivrosController;
