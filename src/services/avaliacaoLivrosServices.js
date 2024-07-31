afa8c12655deceb9629ec1aa45c8a7777f02f479
Dirceusljr <dirceusljr@gmail.com>
2024-07-31 18:43:07 -0300

    refactor: :recycle: refactor: refatorando a rota avaliacao-livros

--- src/services/avaliacaoLivrosServices.js
+++ src/services/avaliacaoLivrosServices.js
@@ -1,9 +1,11 @@
+import AvaliacaoLivrosRepository from "../repositories/avaliacaoLivrosRepository.js";
 import Services from "./services.js";
 
+const avaliacaoLivrosRepository = new AvaliacaoLivrosRepository();
 
 class AvaliacaoLivrosServices extends Services {
     constructor() {
-        super('avaliacaoLivro');
+        super(avaliacaoLivrosRepository);
     }
 
 }
