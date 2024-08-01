93ae09408905c58b26a02ab8e00f55c848f619e1
Dirceusljr <dirceusljr@gmail.com>
2024-08-01 15:54:23 -0300

    refactor: :recycle: refactor: refatorando rota livros-desejados

---
+++ src/repositories/livrosDesejadosRepository.js
@@ -0,0 +1,9 @@
+import Repository from "./repository.js";
+
+class LivrosDesejadosRepository extends Repository {
+    constructor() {
+        super('livroDesejado');
+    }
+}
+
+export default LivrosDesejadosRepository;
