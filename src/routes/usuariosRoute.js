c20908f448b6b1f2c5af2248cf1ec18441959d87
Dirceusljr <dirceusljr@gmail.com>
2024-08-02 15:26:41 -0300

    feat: :sparkles: add: tratamento de erros na rota usuários

--- src/routes/usuariosRoute.js
+++ src/routes/usuariosRoute.js
@@ -2,13 +2,16 @@ import { Router } from "express";
 import UsuariosController from "../controllers/usuariosController.js";
 import LivrosController from "../controllers/livrosController.js";
 import autenticado from "../middlewares/autenticado.js";
+import { celebrate } from "celebrate";
+import usuarioNovoSchema from "../utils/schemas/usuarioNovoSchema.js";
+import usuarioExistenteSchema from "../utils/schemas/usuarioExistenteSchema.js";
 
 const router = Router();
 
 const usuariosController = new UsuariosController();
 const livrosController = new LivrosController();
 
-router.post("/usuarios", (req, res) => usuariosController.criaNovo(req, res));
+router.post("/usuarios", celebrate(usuarioNovoSchema), (req, res) => usuariosController.criaNovo(req, res));
 
 router.use(autenticado);
 
@@ -17,7 +20,7 @@ router
   .get("/usuarios/:id", (req, res) => usuariosController.pegaUmPorId(req, res))
   .get("/usuarios/:usuarioId/livros", (req, res) => livrosController.pegaLivrosPorUsuarioId(req, res))
   .post("/usuarios/:usuarioId/livros", (req, res) => livrosController.cadastraLivroParaUsuario(req, res))
-  .put("/usuarios/:id", (req, res) => usuariosController.atualiza(req, res))
+  .put("/usuarios/:id", celebrate(usuarioExistenteSchema), (req, res) => usuariosController.atualiza(req, res))
   .put("/usuarios/:usuarioId/livros/:id", (req, res) => livrosController.atualizaLivroDoUsuario(req, res))
   .delete("/usuarios/:id",  (req, res) => usuariosController.exclui(req, res))
   .delete("/usuarios/:usuarioId/livros/:id", (req, res) => livrosController.excluiLivroDoUsuario(req, res));
