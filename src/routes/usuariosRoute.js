ee63176b125cdf8aeb3df3e75ed628a61f683480
Dirceusljr <dirceusljr@gmail.com>
2024-08-03 18:45:50 -0300

    feat: :sparkles: add: função de validação de schema

--- src/routes/usuariosRoute.js
+++ src/routes/usuariosRoute.js
@@ -2,16 +2,19 @@ import { Router } from "express";
 import UsuariosController from "../controllers/usuariosController.js";
 import LivrosController from "../controllers/livrosController.js";
 import autenticado from "../middlewares/autenticado.js";
-import { celebrate } from "celebrate";
-import usuarioNovoSchema from "../utils/schemas/usuarioNovoSchema.js";
-import usuarioExistenteSchema from "../utils/schemas/usuarioExistenteSchema.js";
+import { celebrate, Segments } from "celebrate";
+
+import usuarioSchema from "../utils/schemas/usuarioSchema.js";
+import validadorSchema from "../utils/schemas/validadorSchema.js";
 
 const router = Router();
 
 const usuariosController = new UsuariosController();
 const livrosController = new LivrosController();
 
-router.post("/usuarios", celebrate(usuarioNovoSchema), (req, res) => usuariosController.criaNovo(req, res));
+
+
+router.post("/usuarios",celebrate({[Segments.BODY]: validadorSchema(usuarioSchema)}),  (req, res) => usuariosController.criaNovo(req, res));
 
 router.use(autenticado);
 
@@ -20,7 +23,7 @@ router
   .get("/usuarios/:id", (req, res) => usuariosController.pegaUmPorId(req, res))
   .get("/usuarios/:usuarioId/livros", (req, res) => livrosController.pegaLivrosPorUsuarioId(req, res))
   .post("/usuarios/:usuarioId/livros", (req, res) => livrosController.cadastraLivroParaUsuario(req, res))
-  .put("/usuarios/:id", celebrate(usuarioExistenteSchema), (req, res) => usuariosController.atualiza(req, res))
+  .put("/usuarios/:id", celebrate({[Segments.BODY]: validadorSchema(usuarioSchema, "senha", true)}), (req, res) => usuariosController.atualiza(req, res))
   .put("/usuarios/:usuarioId/livros/:id", (req, res) => livrosController.atualizaLivroDoUsuario(req, res))
   .delete("/usuarios/:id",  (req, res) => usuariosController.exclui(req, res))
   .delete("/usuarios/:usuarioId/livros/:id", (req, res) => livrosController.excluiLivroDoUsuario(req, res));
