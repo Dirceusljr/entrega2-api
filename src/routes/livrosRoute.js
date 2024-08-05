d27808f9d8c5b0f699df932e83511ce83a35649a
heloisacativo <heloisacativo@gmail.com>
2024-08-05 12:58:09 -0300

    adiciona validações com celebrate

--- src/routes/livrosRoute.js
+++ src/routes/livrosRoute.js
@@ -1,15 +1,18 @@
 import { Router } from "express";
 import LivrosController from "../controllers/livrosController.js";
-
+import { validacaoCriarLivro, gerenciadorDeErros, atualizarLivroDoUsuario, validacaoParametroLivroId } from '../middlewares/index.js';
 const livrosController = new LivrosController();
+import { celebrate } from 'celebrate';
 
 const router = Router();
 
 router
     .get('/livros', (req, res) => livrosController.pegaTodos(req, res))
-    .get('/livros/:id', (req, res) => livrosController.pegaUmPorId(req, res))
-    .post('/livros', (req, res) => livrosController.criaNovo(req, res))
-    .put('/livros/:id', (req, res) => livrosController.atualiza(req, res))
-    .delete('/livros/:id', (req, res) => livrosController.exclui(req, res))
+    .get('/livros/:id', celebrate(validacaoParametroLivroId), (req, res) => livrosController.pegaUmPorId(req, res))
+    .post('/livros', celebrate(validacaoCriarLivro), (req, res) => livrosController.criaNovo(req, res))
+    .put('/livros/:id', celebrate(atualizarLivroDoUsuario), celebrate(validacaoParametroLivroId), (req, res) => livrosController.atualiza(req, res))
+    .delete('/livros/:id', celebrate(validacaoParametroLivroId), (req, res) => livrosController.exclui(req, res))
+
+router.use(gerenciadorDeErros);
 
 export default router;
--- src/routes/usuarioCargosRoute.js
+++ src/routes/usuarioCargosRoute.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import UsuarioCargosController from "../controllers/usuarioCargosController.js";
-
+import { celebrate } from "celebrate";
+import { gerenciadorDeErros, validacaoUsuarioCargos, validacaoParametroUsuarioId } from '../middlewares/index.js';
 
 const usuarioCargosController = new UsuarioCargosController();
 
@@ -8,10 +9,11 @@ const router = Router();
 
 router
     .get('/usuario-cargos', (req, res) => usuarioCargosController.pegaTodos(req, res))
-    .get('/usuario-cargos/:id', (req, res) => usuarioCargosController.pegaUmPorId(req, res))
-    .post('/usuario-cargos', (req, res) => usuarioCargosController.criaNovo(req, res))
-    .put('/usuario-cargos/:id', (req, res) => usuarioCargosController.atualiza(req, res))
-    .delete('/usuario-cargos/:id', (req, res) => usuarioCargosController.exclui(req, res))
+    .get('/usuario-cargos/:id', celebrate(validacaoParametroUsuarioId), (req, res) => usuarioCargosController.pegaUmPorId(req, res))
+    .post('/usuario-cargos', celebrate(validacaoUsuarioCargos), (req, res) => usuarioCargosController.criaNovo(req, res))
+    .put('/usuario-cargos/:id', celebrate(validacaoParametroUsuarioId), (req, res) => usuarioCargosController.atualiza(req, res))
+    .delete('/usuario-cargos/:id', celebrate(validacaoParametroUsuarioId), (req, res) => usuarioCargosController.exclui(req, res))
 
+    router.use(gerenciadorDeErros);
 
 export default router;
