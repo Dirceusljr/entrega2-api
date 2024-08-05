6c87d2b20df496f4248560d77965fa22e5518842
heloisacativo <heloisacativo@gmail.com>
2024-08-05 12:26:28 -0300

    atualizar e deletar verificacao de dados e registros

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -40,6 +40,7 @@ class Controller {
 
         try {
             const novoRegistro = await this.entidadeService.criaNovoRegistro(dados);
+
             return res.status(201).send({ message: "Registro criado com sucesso!", novoRegistro});
         } catch (erro) {
             return res.status(500).json({ erro: erro.message});
@@ -49,9 +50,22 @@ class Controller {
     async atualiza(req, res) {
         const where = req.params;
         const dadosAtualizados = req.body;
-        try {
-            const registroAtualizado = await this.entidadeService.atualizaRegistro(dadosAtualizados, where);
 
+        try {
+            if (Object.keys(dadosAtualizados).length === 0) {
+                return res.status(400).json({ message: "Nenhum dado fornecido pra atualização. Portanto, nada foi mudado." });
+            }
+            
+            const registroAtual = await this.entidadeService.pegaUmRegistroPorId(where.id);            
+            const dadosIguais = Object.keys(dadosAtualizados).every(
+                (key) => dadosAtualizados[key] === registroAtual[key]
+            );
+
+            if(dadosIguais) {
+                return res.status(400).json({ message: "Os dados fornecidos são iguais aos atuais. Nenhuma alteração foi feita." });
+            }
+
+            const registroAtualizado = await this.entidadeService.atualizaRegistro(dadosAtualizados, where);
             return res.status(200).send({ message: "Registro atualizado com sucesso!", registroAtualizado});
         } catch (erro) {
             return res.status(500).json({ erro: erro.message});
@@ -60,9 +74,14 @@ class Controller {
     
     async exclui(req, res) {
         const where = req.params;
-        try {
-            const registroExcluido = await this.entidadeService.excluiRegistro(where);
 
+        try {
+            const registroExistente = await this.entidadeService.pegaUmRegistroPorId(where.id);
+
+            if(!registroExistente) {
+                return res.status(404).json({ message: "Registro não encontrado. Nenhuma exclusão realizada." });
+        }
+            const registroExcluido = await this.entidadeService.excluiRegistro(where);
             return res.status(200).send({ message: "Registro excluído com sucesso!", registroExcluido});
         } catch (erro) {
             return res.status(500).json({ erro: erro.message});
