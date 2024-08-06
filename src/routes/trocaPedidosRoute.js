0189a09e50dfcb5818c6c290ce7a3a0e00896685
Dirceusljr <dirceusljr@gmail.com>
2024-08-06 16:28:44 -0300

    fix: :bug: Fix: corrigindo bugs nos erros e na autorização

--- src/routes/trocaPedidosRoute.js
+++ src/routes/trocaPedidosRoute.js
@@ -3,6 +3,8 @@ import TrocaPedidosController from "../controllers/trocaPedidosController.js";
 import paginar from "../middlewares/paginar.js";
 import { celebrate } from 'celebrate';
 import { gerenciadorDeErros, validacaoTrocaPedido, validacaoTrocaPedidoParametro } from '../utils/index.js'
+import { atualizacaoTrocaPedido } from "../utils/validacao/trocaPedidosValidacao.js";
+import autorizacao from "../middlewares/autorizacao.js";
 
 const trocaPedidosController = new TrocaPedidosController();
 
@@ -12,8 +14,8 @@ router
     .get('/troca-pedidos', (req, res, next) => trocaPedidosController.pegaTodos(req, res, next), paginar)
     .get('/troca-pedidos/:id', celebrate(validacaoTrocaPedidoParametro), (req, res) => trocaPedidosController.pegaUmPorId(req, res))
     .post('/troca-pedidos', celebrate(validacaoTrocaPedido), (req, res) => trocaPedidosController.criaNovo(req, res))
-    .put('/troca-pedidos/:id', celebrate(validacaoTrocaPedidoParametro), (req, res) => trocaPedidosController.atualiza(req, res))
-    .delete('/troca-pedidos/:id', celebrate(validacaoTrocaPedidoParametro), (req, res) => trocaPedidosController.exclui(req, res))
+    .put('/troca-pedidos/:id', celebrate(validacaoTrocaPedidoParametro), celebrate(atualizacaoTrocaPedido),  (req, res) => trocaPedidosController.atualiza(req, res))
+    .delete('/troca-pedidos/:id', autorizacao(["Dev"])  , celebrate(validacaoTrocaPedidoParametro), (req, res) => trocaPedidosController.exclui(req, res))
 
     router.use(gerenciadorDeErros);
 
--- src/utils/validacao/reputacaoValidacao.js
+++ src/utils/validacao/reputacaoValidacao.js
@@ -6,15 +6,17 @@ const validacaoCriarReputacao = {
         trocaId: Joi.string().min(2).max(800).required().messages(mensagensCustomizadas('trocaId')),
         usuarioDeId: Joi.string().min(2).max(800).required().messages(mensagensCustomizadas('usuarioDeId')),
         usuarioParaId: Joi.string().min(2).max(800).required().messages(mensagensCustomizadas('usuarioParaId')),
-        avaliacao: Joi.number().min(0).max(10).precision(1).required().messages(mensagensCustomizadas('avaliacao')),
-        comentario: Joi.string().min(2).max(800).required().messages(mensagensCustomizadas('comentario')),
+        avaliacao: Joi.number().min(0).max(5).precision(1).required().messages(mensagensCustomizadas('avaliacao')),
+        comentario: Joi.string().min(2).max(800).messages(mensagensCustomizadas('comentario')),
     }).options({ abortEarly: false})
 }
 
 const validacaoAtualizarReputacao = {
     [Segments.BODY]: Joi.object().keys({
-        trocaId: Joi.string().min(2).max(800).messages(mensagensCustomizadas('trocaId')),
-        avaliacao: Joi.number().min(0).max(10).precision(1).messages(mensagensCustomizadas('avaliacao')),
+        trocaId: Joi.string().min(2).max(800).required().messages(mensagensCustomizadas('trocaId')),
+        usuarioDeId: Joi.string().min(2).max(800).required().messages(mensagensCustomizadas('usuarioDeId')),
+        usuarioParaId: Joi.string().min(2).max(800).required().messages(mensagensCustomizadas('usuarioParaId')),
+        avaliacao: Joi.number().min(0).max(5).precision(1).required().messages(mensagensCustomizadas('avaliacao')),
         comentario: Joi.string().min(2).max(800).messages(mensagensCustomizadas('comentario')),
     }).options({ abortEarly: false})
 }
