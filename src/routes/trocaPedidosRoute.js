14459aa17e5f7bbc25157860c4b953f02d367a24
Dirceusljr <dirceusljr@gmail.com>
2024-08-21 20:31:26 -0300

    fix: :bug: Fix: removendo autorizacao

--- src/routes/trocaPedidosRoute.js
+++ src/routes/trocaPedidosRoute.js
@@ -15,7 +15,7 @@ router
     .get('/troca-pedidos/:id', celebrate(validacaoTrocaPedidoParametro), (req, res) => trocaPedidosController.pegaUmPorId(req, res))
     .post('/troca-pedidos', celebrate(validacaoTrocaPedido), (req, res) => trocaPedidosController.criaNovo(req, res))
     .put('/troca-pedidos/:id', celebrate(validacaoTrocaPedidoParametro), celebrate(atualizacaoTrocaPedido),  (req, res) => trocaPedidosController.atualiza(req, res))
-    .delete('/troca-pedidos/:id', autorizacao(["Dev"])  , celebrate(validacaoTrocaPedidoParametro), (req, res) => trocaPedidosController.exclui(req, res))
+    .delete('/troca-pedidos/:id', celebrate(validacaoTrocaPedidoParametro), (req, res) => trocaPedidosController.exclui(req, res))
 
     router.use(gerenciadorDeErros);
 
